f7462c5437cf739c4b92fea5c65dcd9d4395383c
Bruce Ruffin <bruffin1221@gmail.com>
2019-11-07 17:55:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,13 +7,18 @@ class EmailAddressParser
 
 attr_accessor :email_addresses
 
-def initialize=(email_addresses)
+def initialize(email_addresses)
   @email_addresses=email_addresses
 end
+
+def parse
+  @email_addresses.split(",")
+end
+
 
 end
 
 email_addresses = "avi@test.com, arel@test.com"
-parser = EmailAddressParser.parser(email_addresses)
+parser = EmailAddressParser.new(email_addresses)
 
 parser.parse
